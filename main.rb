ecaeb5478fff6d3ffcc1b19d7c03d5eab358ea27
かずき <skaz0110@yahoo.co.jp>
2020-12-24 23:50:53 +0900

    マルバツゲーム

--- main.rb
+++ main.rb
@@ -1,4 +1,17 @@
-require "./Brave"
+class ClassName
+    
+CheckBoard = "123\n456\n789\n"
 
-brave = Brave.new
-puts brave.braveName("勇者")
+9.times do |i|
+puts CheckBoard
+j = i%2
+print "どこ？"
+input = gets.strip
+if j = 0
+    CheckBoard.gsub!(/#{input}/,"○")
+else
+    CheckBoard.gsub!(/#{input}/,"×")
+end    
+end
+gets
+end
